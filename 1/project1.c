c082858338086280ab7b061b34fd0b405665ea02
Jacob Duvall <jdaleduvall@gmail.com>
2018-09-27 23:57:00 +0000

    third commit

--- 1/project1.c
+++ 1/project1.c
@@ -1,6 +1,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define MAX_BUFFER 1024
 #define MAX_ARGS 64
@@ -12,6 +13,9 @@ int main (int argc, char ** argv)
 	char * args[MAX_ARGS];
 	char ** arg;
 	char * prompt = "==>";
+	extern char **environ;
+	char stringCat[1000];
+	char directory[1000];
 
 	while (!feof(stdin))
 	{
@@ -20,7 +24,7 @@ int main (int argc, char ** argv)
 		{
 			arg = args;
 			*arg++ = strtok(buf, SEPARATORS);
-			while ((*arg++ = strok(NULL, SEPARATORS)));
+			while ((*arg++ = strtok(NULL, SEPARATORS)));
 
 			if (args[0]) {
 
@@ -29,6 +33,7 @@ int main (int argc, char ** argv)
 					system("clear");
 					continue;
 				}
+				
 				if (!strcmp(args[0], "esc"))
 				{
 					exit(0);
@@ -42,21 +47,24 @@ int main (int argc, char ** argv)
 					}
 					else
 					{
-					
+						printf("%s", args[1]);
 						continue;
 					}
 				}
 				if (!strcmp(args[0], "environ"))
 				{
+					char ** env = environ;
+					while (*env) printf("%s\n", *env++);
 
+					continue;
 				}
 				if (!strcmp(args[0], "ditto"))
 				{
-
+					
 				}
 				if (!strcmp(args[0], "help"))
 				{
-
+					system("cat README");
 				}
 				if (!strcmp(args[0], "mimic"))
 				{
@@ -64,7 +72,8 @@ int main (int argc, char ** argv)
 				}
 				if (!strcmp(args[0], "erase"))
 				{
-
+					remove(args[1]);
+					continue;
 				}
 				if (!strcmp(args[0], "morph"))
 				{
@@ -72,9 +81,34 @@ int main (int argc, char ** argv)
 				}
 				if (!strcmp(args[0], "chdir"))
 				{
-
+					if(args[1] == NULL)
+					{
+						getcwd(directory, sizeof(directory));
+						printf("%s\n", directory);
+						directory[0] = 0;	
+					}
+					else 
+					{
+						strcat(directory, "cd ");
+						strcat(directory, args[1]);
+						printf("%s\n", args[1]);
+						printf("%s\n", directory);
+						system(directory);
+					}
+					continue;
 				}
 				else
 				{
 					
+			
 				}
+				arg = args;
+				while (*arg) fprintf(stdout, "%s ", *arg++);
+				fputs("\n", stdout);
+			}
+
+		}
+	}
+	return 0;
+}
+
